61c9d71c5d0b2dc326a092438d5a9bfb38f94bcf
Vítor Caitité <vitorgabriel1000@hotmail.com>
2021-11-01 20:05:02 -0300

    Adding the targets.

--- shooting_range.py
+++ shooting_range.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -18,10 +19,18 @@ text_2 = pygame.image.load("resources/text_2.png")
 text_2 = pygame.transform.scale(text_2, (32*3, 56*3))
 text_3 = pygame.image.load("resources/text_3.png")
 text_3 = pygame.transform.scale(text_3, (32*3, 56*3))
+crosshair = pygame.image.load("resources/crosshair_red_small.png")
+target_surface = pygame.image.load("resources/duck_target_white.png")
+
 land_position_y = 560
 land_speed_y = 0.5
 water_position_y = 640
 water_speed_y = 0.5
+
+target_list = [None] * 20
+for target in range(20):
+    target_rect = target_surface.get_rect(center=(random.randrange(80, 1200), random.randrange(120, 600)))
+    target_list[target] = target_rect
 
 # Game Introduction
 screen.blit(sky_bg, (0,0))
@@ -43,8 +52,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEMOTION:
+            crosshair_rect = crosshair.get_rect(center=event.pos)
 
     screen.blit(sky_bg, (0,0))
+
+    for target in target_list:
+        screen.blit(target_surface, target)
+
     land_position_y -= land_speed_y
     if land_position_y <= 520 or land_position_y >= 600:
         land_speed_y *= -1
@@ -53,11 +68,13 @@ while True:
     if water_position_y <= 600 or water_position_y >= 680:
         water_speed_y *= -1
     screen.blit(water_bg, (0, water_position_y))
+
     screen.blit(cloud1_bg, (300, 100))
     screen.blit(cloud2_bg, (100, 150))
     screen.blit(cloud1_bg, (800, 200))
     screen.blit(cloud2_bg, (1000, 170))
     screen.blit(cloud1_bg, (600, 50))
+    screen.blit(crosshair, crosshair_rect)
     pygame.display.update()
     clock.tick(120)
 
